87ea48f4bab38a6f12806cd82ba783dd24005d3f
clszzyh <clszzyh@gmail.com>
2020-11-30 18:56:33 +0800

    Add stage map
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- lib/battle_city/stage.ex
+++ lib/battle_city/stage.ex
@@ -1,11 +1,20 @@
 defmodule BattleCity.Stage do
   @moduledoc false
 
-  @type level :: integer
+  @type map_data :: [term()]
+  @type bot :: term()
 
-  @type t :: %__MODULE__{level: level()}
+  @type t :: %__MODULE__{
+          name: binary(),
+          difficulty: integer(),
+          map: map_data,
+          bots: [bot]
+        }
 
   defstruct [
-    :level
+    :name,
+    :difficulty,
+    :map,
+    :bots
   ]
 end
