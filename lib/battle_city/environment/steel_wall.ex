7746c3dd156414dc6549d587fdef58824be247de
clszzyh <clszzyh@gmail.com>
2020-12-01 15:07:59 +0800

    Support SteelWall
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- lib/battle_city/environment/steel_wall.ex
+++ lib/battle_city/environment/steel_wall.ex
@@ -5,8 +5,20 @@ defmodule BattleCity.Environment.SteelWall do
     health: 4,
     enter?: false
 
-  # @impl true
-  # def handle_init(%{stage: _stage} = o) do
-  #   o
-  # end
+  @health_map %{
+    "f" => 1,
+    "3" => 2,
+    "c" => 3,
+    "5" => 4,
+    "a" => 5
+  }
+
+  @stages Map.keys(@health_map)
+
+  @impl true
+  def handle_init(%{stage: stage}) when stage in @stages do
+    %{stage: stage, health: @health_map[stage]}
+  end
+
+  def handle_init(map), do: raise(CompileError, description: "Error #{inspect(map)}")
 end
