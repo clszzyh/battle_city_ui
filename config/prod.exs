39da6116be091065bc106881b6246f79e5fe2c20
clszzyh <clszzyh@gmail.com>
2020-12-18 20:03:36 +0800

    http
    Signed-off-by: clszzyh <clszzyh@gmail.com>

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ use Mix.Config
 config :battle_city, BattleCityWeb.Endpoint,
   url: [scheme: "https", host: "#{System.get_env("APP_NAME")}.gigalixirapp.com", port: 443],
   check_origin: ["//*.gigalixirapp.com", "//*.clszzyh.com"],
+  http: [:inet6, port: System.get_env("PORT") || 4000],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
